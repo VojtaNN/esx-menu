574f4af1ad1d4978fe0e9fb915cb1d95d300ec02
VojtaNN <61760213+VojtaNN@users.noreply.github.com>
2022-08-26 15:10:36 +0200

    Update __resource.lua

--- esx_menu_default/__resource.lua
+++ esx_menu_default/__resource.lua
@@ -1,7 +1,5 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'ESX Menu Default'
---- redesigned by PEPEGPD FOR EILEITHYIA
---- if you have this you are choosen one
 version '1.0.4'
 client_scripts {
 	'@es_extended/client/wrapper.lua',
